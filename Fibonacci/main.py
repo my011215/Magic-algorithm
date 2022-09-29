e68fe18e60a4d962dfefb790ade1b749ccdc8a2d
returnToInnocence <103014488+returnToInnocence@users.noreply.github.com>
2022-09-29 15:49:26 +0800

    Li

--- Fibonacci/main.py
+++ Fibonacci/main.py
@@ -36,8 +36,50 @@ def FibonacciEquation(num):
 	result = x*(y**num-z**num)
 	print(result)
 
+# 递归
+def Fibonacci_Recursion_tool(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return Fibonacci_Recursion_tool(n - 1) + Fibonacci_Recursion_tool(n - 2)
+
+i = int(input('请输入求第几个斐波那契数：'))
+print('结果为：' + str(Fibonacci_Recursion_tool(i)))
+
+# 两个n阶矩阵相乘
+def matrix_multiplication(n, A, B):
+    C = []
+    for line in range(n):
+        line_arr = []
+        for column in range(n):
+            item = 0
+            for i in range(n):
+                item += A[line][i] * B[i][column]
+            line_arr.append(item)
+        C.append(line_arr)
+    return C
+
+# 矩阵快速幂求解，所求元素为A^{n-1} 中的 A[0][0], 或 A^n 中的 A[0][1]
+def Fib_matrix_power(n):
+    # 检查输入
+    if n < 2: return n
+    A = [[1, 1], [1, 0]]
+    result = [[1, 0], [0, 1]]
+    matrix_n = 2
+    while n > 0:
+        # 判断最后一位是否为1，即可知奇偶
+        if n & 1:
+            result = matrix_multiplication(matrix_n, result, A)
+        A = matrix_multiplication(matrix_n, A, A)
+        n //= 2
+        # n = n >> 1
+    return result[0][1]
 
 
+i = int(input('请输入求第几个斐波那契数：'))
+print('结果为：' + str(Fib_matrix_power(i)))
 
 FibonacciD(7)
 FibonacciDImproment(7)
